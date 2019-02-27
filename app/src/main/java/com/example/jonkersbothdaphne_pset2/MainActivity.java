f8b95c9bbc8fbf30f1d262018bc33aa4d1e68062
Daphne JB <d.v.jonkersboth@amc.uva.nl>
2019-02-27 19:50:17 +0100

    Created instruction activity

--- app/src/main/java/com/example/jonkersbothdaphne_pset2/MainActivity.java
+++ app/src/main/java/com/example/jonkersbothdaphne_pset2/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.jonkersbothdaphne_pset2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
     public void startClicked(View view) {
+        System.out.println("test");
+        startActivity(new Intent(this, Welcome.class));
     }
 }
